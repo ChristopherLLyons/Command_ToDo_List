fd8a72a6de2b9c04aa72b3b05d2808aae56f0da3
Christopher Lyons <christopherlyons916@gmail.com>
2019-09-12 13:19:32 -0500

    Added JavaScript file to the HTML and outlined the JavaScript file.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/* 
+-Container to hold the Todos
+-Way to ask the user for input
+-Keep asking the user for input until they type that they want ton quit
+-Add a Todo
+-List our Todos
+-Remove a Todo
+*/
